80c010e97bc308f7e04821800c15150d0cf02ffd
Rafal Kowalski <rafal.kowalski@snowflake.com>
2026-01-26 14:17:29 +0100

    Extract common IT base (#210)

--- jdbc/src/test/java/com/snowflake/jdbc/SnowflakeQueryTest.java
+++ jdbc/src/test/java/com/snowflake/jdbc/SnowflakeQueryTest.java
@@ -4,82 +4,17 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
-import java.io.InputStream;
-import java.io.InputStreamReader;
 import java.sql.Connection;
-import java.sql.DriverManager;
 import java.sql.ResultSet;
 import java.sql.Statement;
-import java.util.Properties;
-import org.json.JSONObject;
-import org.json.JSONTokener;
 import org.junit.jupiter.api.Test;
 
 /** Tests for executing queries through the Snowflake JDBC Driver */
-public class SnowflakeQueryTest {
-
-  private Properties loadConnectionProperties() throws Exception {
-    // Load parameters.json from test resources
-    String paramPath = System.getenv("PARAMETER_PATH");
-    if (paramPath == null) {
-      paramPath = "/parameters.json";
-    }
-    InputStream input = new java.io.FileInputStream(paramPath);
-    if (input == null) {
-      throw new RuntimeException("Could not find parameters.json in test resources");
-    }
-
-    JSONObject params = new JSONObject(new JSONTokener(new InputStreamReader(input)));
-    params = params.getJSONObject("testconnection");
-
-    Properties props = new Properties();
-    props.setProperty("user", params.getString("SNOWFLAKE_TEST_USER"));
-    props.setProperty("password", params.getString("SNOWFLAKE_TEST_PASSWORD"));
-    props.setProperty("db", params.getString("SNOWFLAKE_TEST_DATABASE"));
-    props.setProperty("schema", params.getString("SNOWFLAKE_TEST_SCHEMA"));
-    props.setProperty("warehouse", params.getString("SNOWFLAKE_TEST_WAREHOUSE"));
-    props.setProperty("account", params.getString("SNOWFLAKE_TEST_ACCOUNT"));
-
-    // Add optional parameters if specified
-    if (params.has("SNOWFLAKE_TEST_PORT")) {
-      props.setProperty("port", String.valueOf(params.getInt("SNOWFLAKE_TEST_PORT")));
-    }
-
-    if (params.has("SNOWFLAKE_TEST_ROLE")) {
-      props.setProperty("role", params.getString("SNOWFLAKE_TEST_ROLE"));
-    }
-
-    if (params.has("SNOWFLAKE_TEST_SERVER_URL")) {
-      props.setProperty("server_url", params.getString("SNOWFLAKE_TEST_SERVER_URL"));
-    }
-
-    if (params.has("SNOWFLAKE_TEST_HOST")) {
-      props.setProperty("host", params.getString("SNOWFLAKE_TEST_HOST"));
-    }
-
-    if (params.has("SNOWFLAKE_TEST_PROTOCOL")) {
-      props.setProperty("protocol", params.getString("SNOWFLAKE_TEST_PROTOCOL"));
-    }
-
-    return props;
-  }
+public class SnowflakeQueryTest extends SnowflakeIntegrationTestBase {
 
   @Test
   public void testSimpleSelect() throws Exception {
-    // Load connection properties
-    Properties props = loadConnectionProperties();
-    String defaultUrl =
-        "jdbc:snowflake://" + props.getProperty("account") + ".snowflakecomputing.com";
-    if (props.getProperty("port") != null) {
-      defaultUrl += ":" + props.getProperty("port");
-    }
-    String url = props.getProperty("url", defaultUrl);
-
-    // Create connection
-    SnowflakeDriver.empty();
-    Connection conn = DriverManager.getConnection(url, props);
-
-    try {
+    try (Connection conn = openConnection()) {
       // Create and execute statement
       Statement stmt = conn.createStatement();
       ResultSet rs = stmt.executeQuery("SELECT 1");
@@ -92,8 +27,6 @@ public class SnowflakeQueryTest {
       // Clean up
       rs.close();
       stmt.close();
-    } finally {
-      conn.close();
     }
   }
 }
--- jdbc/src/test/java/com/snowflake/jdbc/SnowflakeResultSetGettersTest.java
+++ jdbc/src/test/java/com/snowflake/jdbc/SnowflakeResultSetGettersTest.java
@@ -5,77 +5,13 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
-import java.io.InputStream;
-import java.io.InputStreamReader;
 import java.math.BigDecimal;
 import java.sql.Connection;
-import java.sql.DriverManager;
 import java.sql.ResultSet;
 import java.sql.Statement;
-import java.util.Properties;
-import org.json.JSONObject;
-import org.json.JSONTokener;
 import org.junit.jupiter.api.Test;
 
-public class SnowflakeResultSetGettersTest {
-
-  private Properties loadConnectionProperties() throws Exception {
-    // Load parameters.json from test resources
-    String paramPath = System.getenv("PARAMETER_PATH");
-    if (paramPath == null) {
-      paramPath = "/parameters.json";
-    }
-    InputStream input = new java.io.FileInputStream(paramPath);
-    if (input == null) {
-      throw new RuntimeException("Could not find parameters.json in test resources");
-    }
-
-    JSONObject params = new JSONObject(new JSONTokener(new InputStreamReader(input)));
-    params = params.getJSONObject("testconnection");
-
-    Properties props = new Properties();
-    props.setProperty("user", params.getString("SNOWFLAKE_TEST_USER"));
-    props.setProperty("password", params.getString("SNOWFLAKE_TEST_PASSWORD"));
-    props.setProperty("db", params.getString("SNOWFLAKE_TEST_DATABASE"));
-    props.setProperty("schema", params.getString("SNOWFLAKE_TEST_SCHEMA"));
-    props.setProperty("warehouse", params.getString("SNOWFLAKE_TEST_WAREHOUSE"));
-    props.setProperty("account", params.getString("SNOWFLAKE_TEST_ACCOUNT"));
-
-    // Add optional parameters if specified
-    if (params.has("SNOWFLAKE_TEST_PORT")) {
-      props.setProperty("port", String.valueOf(params.getInt("SNOWFLAKE_TEST_PORT")));
-    }
-
-    if (params.has("SNOWFLAKE_TEST_ROLE")) {
-      props.setProperty("role", params.getString("SNOWFLAKE_TEST_ROLE"));
-    }
-
-    if (params.has("SNOWFLAKE_TEST_SERVER_URL")) {
-      props.setProperty("server_url", params.getString("SNOWFLAKE_TEST_SERVER_URL"));
-    }
-
-    if (params.has("SNOWFLAKE_TEST_HOST")) {
-      props.setProperty("host", params.getString("SNOWFLAKE_TEST_HOST"));
-    }
-
-    if (params.has("SNOWFLAKE_TEST_PROTOCOL")) {
-      props.setProperty("protocol", params.getString("SNOWFLAKE_TEST_PROTOCOL"));
-    }
-
-    return props;
-  }
-
-  private Connection openConnection() throws Exception {
-    Properties props = loadConnectionProperties();
-    String defaultUrl =
-        "jdbc:snowflake://" + props.getProperty("account") + ".snowflakecomputing.com";
-    if (props.getProperty("port") != null) {
-      defaultUrl += ":" + props.getProperty("port");
-    }
-    String url = props.getProperty("url", defaultUrl);
-    SnowflakeDriver.empty();
-    return DriverManager.getConnection(url, props);
-  }
+public class SnowflakeResultSetGettersTest extends SnowflakeIntegrationTestBase {
 
   @Test
   public void testGetInt() throws Exception {
